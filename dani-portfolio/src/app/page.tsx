82b4c47399e04eb10550678744c3173385d62531
Andrés Sanhueza <trazo263@gmail.com>
2023-07-10 21:33:40 +0200

    update: randomizing stars

--- dani-portfolio/src/app/page.tsx
+++ dani-portfolio/src/app/page.tsx
@@ -1,39 +1,43 @@
 "use client"
 
 import Image from "next/image"
-import { useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 const baseSize = 120,
+    starsAmount = 12,
     colors = ["white", "pink", "cyan"]
 
-const generateStars = (starsAmount: number) => {
-    const stars = []
-
-    for (let i = 0; i < starsAmount; i++) {
-        const starSize = baseSize + Math.random() * baseSize,
-            starColor = colors[Math.floor(Math.random() * colors.length)]
-
-        stars.push(
-            <div
-                key={i}
-                className="absolute opacity-40 animate-pulse"
-                style={{
-                    top: `${Math.random() * 100}%`,
-                    left: `${Math.random() * 100}%`,
-                    animationDuration: `${2 + Math.random() * 2}s`,
-                }}
-            >
-                <Image src={`/${starColor}-star.svg`} alt="star" width={starSize} height={starSize} />
-            </div>
-        )
-    }
-
-    return stars
-}
-
 export default function Home() {
-    const starsAmount = 12
+    const [stars, setStars] = useState([])
+
+    const generateStars = useCallback((starsAmount: number) => {
+        const newStars = []
+        for (let i = 0; i < starsAmount; i++) {
+            const starSize = baseSize + Math.random() * baseSize,
+                starColor = colors[Math.floor(Math.random() * colors.length)]
     
+            newStars.push(
+                <div
+                    key={i}
+                    className="absolute opacity-40 animate-pulse"
+                    style={{
+                        top: `${Math.random() * 100}%`,
+                        left: `${Math.random() * 100}%`,
+                        animationDuration: `${2 + Math.random() * 2}s`,
+                    }}
+                >
+                    <Image src={`/${starColor}-star.svg`} alt="star" width={starSize} height={starSize} />
+                </div>
+            )
+        }
+        return newStars
+    }, [])
+    
+    useEffect(() => {
+        const newStars = generateStars(starsAmount)
+        setStars(newStars)
+    }, [generateStars])
+
     return (
         <main className="bg-gradient-to-b from-pink to-purple flex h-screen">
             <div className="text-white m-auto w-fit text-center">
@@ -42,9 +46,9 @@ export default function Home() {
                 <p>Product designer</p>
                 <p>6 years into designing digital experiences</p>
             </div>
-            <div className="absolute h-screen w-screen overflow-hidden">{
-                generateStars(starsAmount)
-            }</div>
+            <div className="absolute h-screen w-screen overflow-hidden">
+                {stars}
+            </div>
         </main>
     )
 }
