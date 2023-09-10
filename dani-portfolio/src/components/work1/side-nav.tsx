efabb667a97e59603f3803080ef7b7f432e2be1c
Andrés Sanhueza <trazo263@gmail.com>
2023-09-10 19:29:42 +0200

    update: side navigation fixes

--- dani-portfolio/src/components/work1/side-nav.tsx
+++ dani-portfolio/src/components/work1/side-nav.tsx
@@ -2,18 +2,61 @@
 
 import { useCallback, useEffect, useRef, useState } from "react"
 
-function NavElem({ id, text, scrollY }: { id: string; text: string; scrollY: number }) {
-    const [active, setActive] = useState(false),
+const sections = [
+    {
+        id: "context",
+        text: "Context",
+    },
+    {
+        id: "design-process",
+        text: "Design Process",
+    },
+    {
+        id: "research",
+        text: "Research",
+    },
+    {
+        id: "iteration",
+        text: "Iteration",
+    },
+    {
+        id: "definition",
+        text: "Definition",
+    },
+    {
+        id: "take-away",
+        text: "Take Away",
+    },
+]
+
+function NavElem({ id, text, scrollY, index }: { id: string; text: string; scrollY: number; index: number }) {
+    const [upperBound, setUpperBound] = useState(0),
+        [lowerBound, setLowerBound] = useState(0),
+        active = scrollY > upperBound && scrollY < lowerBound,
         ref = useRef<HTMLAnchorElement>(null),
         deviceHeight = typeof window !== "undefined" ? window.innerHeight : 700,
         textColor = scrollY > deviceHeight - 200 - (ref.current?.offsetTop || 0) ? "text-purple" : "text-white"
+
+    useEffect(() => {
+        if (typeof document !== "undefined") {
+            const { offsetTop, offsetHeight } = document.getElementById(id) || { offsetTop: 0, offsetHeight: 0 },
+                nextSection = document.getElementById(sections[index + 1]?.id)
+
+            if (nextSection) {
+                const { offsetTop: nextOffsetTop } = nextSection
+                setLowerBound(nextOffsetTop)
+            } else setLowerBound(offsetTop + offsetHeight)
+
+            setUpperBound(offsetTop)
+        }
+    }, [])
 
     return (
         <a
             ref={ref}
             href={`#${id}`}
             className={`block hover:text-lightPink px-4 py-2 relative duration-1000 ${textColor} ${
-                active ? "text-bold" : ""
+                active ? "font-bold" : ""
             }`}
         >
             {text}
@@ -44,12 +87,9 @@ export default function SideNav() {
     return (
         <div className="hidden md:block fixed top-[200px] ml-8">
             <p className={`italic mb-2 text-sm duration-1000 ${textColor}`}>On this site</p>
-            <NavElem id="context" text="Context" scrollY={scrollY} />
-            <NavElem id="design-process" text="Design process" scrollY={scrollY} />
-            <NavElem id="research" text="Research" scrollY={scrollY} />
-            <NavElem id="iteration" text="Iteration" scrollY={scrollY} />
-            <NavElem id="definition" text="Definition" scrollY={scrollY} />
-            <NavElem id="take-away" text="Take away" scrollY={scrollY} />
+            {sections.map(({ id, text }, index) => (
+                <NavElem key={id} id={id} text={text} scrollY={scrollY} index={index} />
+            ))}
         </div>
     )
 }
