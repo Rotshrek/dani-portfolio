e0102883a56f98df1e596797a1924a4cb4577c57
Andrés Sanhueza <trazo263@gmail.com>
2023-10-13 19:47:18 +0200

    update: changes to design recommendation sections

--- dani-portfolio/src/components/work1/5-design-recommendations.tsx
+++ dani-portfolio/src/components/work1/5-design-recommendations.tsx
@@ -1,15 +1,38 @@
 "use client"
 
+import Image from "next/image"
+
 export default function DesignRecommendation() {
     return (
-        <div className="h-auto bg-[url('/hands-bg.png')] bg-cover w-full bg-center bg-no-repeat py-20">
-            <div className="bg-white w-3/4 min-w-[320px] max-w-[572px] m-auto py-12 px-8 md:px-20 rounded">
-                <p className="font-bold text-2xl mb-4 text-center">Design recommendation</p>
-                <p className="text-md mb-2">The car license plate should be the first question in the form.</p>
-                <p className="text-md mb-2">We can use our database for autocomplete car information related inputs.</p>
-                <p className="text-md mb-2">
-                    Also give the option to quote without a license plate for those users who prefer it.
-                </p>
+        <div className="h-auto bg-white w-full py-20 ">
+            <div className="relative w-3/4 min-w-[320px] max-w-[572px] m-auto">
+                <Image
+                    className="absolute top-[-40px] left-[-140px] z-0"
+                    src="/pink-cloud-outline.svg"
+                    width={220}
+                    height={100}
+                    alt="pink cloud outline"
+                />
+                <Image
+                    className="absolute bottom-[-40px] right-[-180px] z-0"
+                    src="/blue-cloud.svg"
+                    width={490}
+                    height={190}
+                    alt="blue cloud"
+                />
+                <div className="relative bg-[#FFF] py-12 px-8 md:px-20 rounded shadow-lg">
+                    <p className="font-bold text-2xl mb-4 text-center">💡 Design recommendation</p>
+                    <p className="text-md mb-2">
+                        The <strong>car license plate should be the first question in the form.</strong>
+                    </p>
+                    <p className="text-md mb-2">
+                        We can use our database for autocomplete car information related inputs.
+                    </p>
+                    <p className="text-md mb-2">
+                        Also <strong>give the option to quote without a license plate</strong> for those users who
+                        prefer it.
+                    </p>
+                </div>
             </div>
         </div>
     )
--- dani-portfolio/src/components/work1/7-design-recommendations-2.tsx
+++ dani-portfolio/src/components/work1/7-design-recommendations-2.tsx
@@ -1,17 +1,35 @@
 "use client"
 
+import Image from "next/image"
+
 export default function DesignRecommendation2() {
     return (
-        <div className="h-auto bg-[url('/hands-bg2.png')] bg-cover w-full bg-center bg-no-repeat py-20">
-            <div className="bg-white w-3/4 min-w-[320px] max-w-[572px] m-auto py-12 px-8 md:px-20 rounded">
-                <p className="font-bold text-2xl mb-4 text-center">Design recommendation</p>
-                <p className="text-md mb-2">
-                    <strong>Autocomplete vehicle and owner data</strong>, but not personal data.
-                </p>
-                <p className="text-md">
-                    <strong>Maintain the structure</strong>: Step 1: Car details; Step 2: Owner details and Step 3:
-                    Contact details.
-                </p>
+        <div className="h-auto bg-white w-full py-20 ">
+            <div className="relative w-3/4 min-w-[320px] max-w-[572px] m-auto">
+                <Image
+                    className="absolute top-[-60px] right-[-180px] z-0"
+                    src="/blue-cloud-outline.svg"
+                    width={490}
+                    height={190}
+                    alt="blue cloud outline"
+                />
+                <Image
+                    className="absolute bottom-[-40px] left-[-100px] z-0"
+                    src="/pink-cloud.svg"
+                    width={220}
+                    height={100}
+                    alt="pink cloud"
+                />
+                <div className="relative bg-[#FFF] py-12 px-8 md:px-20 rounded shadow-lg">
+                    <p className="font-bold text-2xl mb-4 text-center">💡 Design recommendation</p>
+                    <p className="text-md mb-2">
+                        <strong>Autocomplete vehicle and owner data</strong>, but not personal data.
+                    </p>
+                    <p className="text-md">
+                        <strong>Maintain the structure</strong>: Step 1: Car details; Step 2: Owner details and Step 3:
+                        Contact details.
+                    </p>
+                </div>
             </div>
         </div>
     )
